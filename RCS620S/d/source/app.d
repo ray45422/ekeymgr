a51c1fd8de8ef0eaefa9e9d043dd1d041b709119
ray45422 <ray45422@gmail.com>
2016-07-29 15:54:32 +0900

    ライブラリ変更
    onyx-serialがエラーを出すのでserial-portに変更
    Arduino-PC間で通信はできたが、Arduino経由ではRCS620Sと通信ができなかった
    シリアル変換ケーブルが届きしだいテストを行う

--- RCS620S/d/source/app.d
+++ RCS620S/d/source/app.d
@@ -1,19 +1,35 @@
 import std.stdio;
-import onyx.serial;
+import core.thread;
+import serial.device;
 
 void main()
 {
-	auto port=OxSerialPort("/dev/ttyACM0", Speed.S115200, Parity.none, 1000);
-	port.open();
-	auto rcs620s=new RCS620S(port);
+	auto port=new SerialPort("/dev/ttyACM0", dur!("msecs")(10), dur!("msecs")(10));
+	port.speed(BaudRate.BR_115200);
+
+	Thread.sleep(dur!("seconds")(2));//Arduinoのリセット待ち
+	ubyte [20] a;
+	ulong ret=0;
 	ubyte[] test=[0xd4, 0x32, 0x02, 0x00, 0x00, 0x00];
 	port.write(test);
+	while(ret==0){
+		try{
+			ret=port.read(a);//Arduino経由だとなぜか読めない(USBシリアル変換ケーブルが届くのを待つ)
+		}catch(TimeoutException e){
+			ret.writeln;
+			Thread.sleep(dur!("msecs")(500));
+			port.write(test);
+			//e.writeln();
+		}
+	}
+	ret.writeln;
+	a.writeln;
 	port.close();
 }
 
 class RCS620S{
-	OxSerialPort port;
-	this(OxSerialPort port){
+	SerialPort port;
+	this(SerialPort port){
 		this.port=port;
 	}
 }
