d12a62817ee1e13bec3669f5798ce10b0aa7f228
ray45422 <ray45422@gmail.com>
2016-11-29 04:05:15 +0900

    add group auth and etc...
    グループ化での認証を実装
    有効期限、曜日時間指定での有効化機能の実装
    DBの変更に合わせて有効回数を削除、また曜日時間指定での時間の指定方法の修正

--- source/auth.d
+++ source/auth.d
@@ -23,37 +23,58 @@ public:
 	}
 	byte auth(string service_name, string id){
 		MysqlResult rows;
+		string select = "SELECT users.user_id,users.user_name,users.disp_name,
+			authdata.id,authdata.auth_id,
+			services.service_name,services.service_id,
+			rooms.room_id,
+			validated_timestamp.timestamp,
+			validated_timestamp_scheduled.days,validated_timestamp_scheduled.start_time,validated_timestamp_scheduled.end_time ";
+		string where = "WHERE (validated_timestamp.timestamp IS NULL OR validated_timestamp.timestamp>=TIMESTAMP(NOW())) AND
+			(validated_timestamp_scheduled.days IS NULL OR
+			(validated_timestamp_scheduled.days LIKE CONCAT('%',DAYOFWEEK(NOW()),'%') AND validated_timestamp_scheduled.start_time<=TIME(NOW()) AND validated_timestamp_scheduled.end_time>=TIME(NOW()))) AND
+			services.service_name='" ~ service_name ~ "' AND authdata.id='" ~ id ~ "' AND authdata.valid_flag=1 AND rooms.room_id='" ~ config.room_id_str ~ "'";
 		try{
 			//rows = mysql.query("SELECT services.*, authdata.*, users.* FROM authdata, services, users WHERE services.service_id=authdata.service_id AND services.service_name=\'" ~ service_name ~ "\' AND authdata.id=\'" ~ service_id ~ "\' AND authdata.user_id = users.user_id AND valid_flag = \'1\'");
-			rows = mysql.query("SELECT users.user_id,users.user_name,users.disp_name,
-				authdata.id,authdata.auth_id,
-				services.service_name,services.service_id,
-				rooms.room_id,
-				available_count.count,
-				validated_timestamp.timestamp,
-				validated_timestamp_scheduled.days,validated_timestamp_scheduled.start_hours,validated_timestamp_scheduled.end_hours
-				FROM rooms
+			rows = mysql.query(select ~ "FROM rooms
 				LEFT JOIN rooms_users ON (rooms.room_id=rooms_users.room_id)
 				JOIN authdata ON (rooms_users.user_id=authdata.user_id)
 				LEFT JOIN (users,services)
 					ON (authdata.user_id=users.user_id AND authdata.service_id=services.service_id)
-				LEFT JOIN (available_count,auth_count)
-					ON (authdata.auth_id=auth_count.auth_id AND available_count.count_id=auth_count.count_id)
 				LEFT JOIN (validated_timestamp,auth_timestamp)
 					ON (authdata.auth_id=auth_timestamp.auth_id AND validated_timestamp.timestamp_id=auth_timestamp.timestamp_id)
 				LEFT JOIN (validated_timestamp_scheduled,auth_timestamp_scheduled)
 					ON (authdata.auth_id=auth_timestamp_scheduled.auth_id AND validated_timestamp_scheduled.timestamp_scheduled_id=auth_timestamp_scheduled.timestamp_scheduled_id) "~
-				"WHERE services.service_name='" ~ service_name ~ "' AND authdata.id='" ~ id ~ "' AND authdata.valid_flag=1 AND rooms.room_id='" ~ config.room_id_str ~ "'"
+				where
 			);
 		}catch(MysqlDatabaseException e){
 			e.msg.writeln;
 			_isSuccess = false;
 			return 1;
 		}
-		if(rows.length != 1){
-			_isSuccess = false;
-			addFailLog(service_name, id);
-			return 64;//合致するIDが見つからない
+		if(rows.length == 0){
+			try{
+				rows = mysql.query(select ~ "FROM rooms
+					LEFT JOIN rooms_groups ON (rooms_groups.room_id=rooms.room_id)
+					LEFT JOIN groups_users ON (rooms_groups.group_id=groups_users.group_id)
+					JOIN authdata ON (groups_users.user_id=authdata.user_id)
+					LEFT JOIN (users,services)
+						ON (authdata.user_id=users.user_id AND authdata.service_id=services.service_id)
+					LEFT JOIN (validated_timestamp,auth_timestamp)
+						ON (authdata.auth_id=auth_timestamp.auth_id AND validated_timestamp.timestamp_id=auth_timestamp.timestamp_id)
+					LEFT JOIN (validated_timestamp_scheduled,auth_timestamp_scheduled)
+						ON (authdata.auth_id=auth_timestamp_scheduled.auth_id AND validated_timestamp_scheduled.timestamp_scheduled_id=auth_timestamp_scheduled.timestamp_scheduled_id) " ~
+					where
+				);
+			}catch(MysqlDatabaseException e){
+				e.msg.writeln;
+				_isSuccess = false;
+				return 1;
+			}
+			if(rows.length == 0){
+				_isSuccess = false;
+				addFailLog(service_name, id);
+				return 64;//合致するIDが見つからない
+			}
 		}
 		lastAuthData = new AuthData(rows.row);
 		//lastAuthData.write();
@@ -92,7 +113,7 @@ private:
 				return false;
 			}
 			string service_id = rows.row["service_id"];
-			mysql.query("INSERT INTO `fail_logs` (`log_id`,`time`,`service_id`,`id`) VALUES(NULL,CURRENT_TIMESTAMP,'" ~ service_id ~ "','" ~ id ~ "')");
+			mysql.query("INSERT INTO `fail_logs` (`log_id`,`time`,`service_id`,`id`,`room_id`) VALUES(NULL,CURRENT_TIMESTAMP,'" ~ service_id ~ "','" ~ id ~ "','" ~ config.room_id_str ~ "')");
 		}catch(MysqlDatabaseException e){
 			e.msg.writeln;
 			return false;
@@ -111,12 +132,6 @@ public:
 		id = result["id"];
 		service_name = result["service_name"];
 		service_id = result["service_id"];
-		count = result["count"];
-	}
-	void update(){
-		if(count == ""){
-			/*回数更新処理*/
-		}
 	}
 	string getName(){
 		return user_name;
@@ -125,7 +140,7 @@ public:
 		return disp_name;
 	}
 	void write(){
-		writefln("user_id: %s, user_name:%s, disp_name:%s, auth_id:%s, room_id:%s, id:%s, service_name:%s, service_id:%s, count:%s",
+		writefln("user_id: %s, user_name:%s, disp_name:%s, auth_id:%s, room_id:%s, id:%s, service_name:%s, service_id:%s",
 			user_id,
 			user_name,
 			disp_name,
@@ -133,8 +148,7 @@ public:
 			room_id,
 			id,
 			service_name,
-			service_id,
-			count);
+			service_id);
 	}
 private:
 	string user_id;
@@ -145,5 +159,4 @@ private:
 	string id;
 	string service_name;
 	string service_id;
-	string count;
 }
