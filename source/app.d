5326383e27fd3141505e632f203e902f9876f266
ray45422 <ray45422@gmail.com>
2017-02-02 12:23:31 +0900

    split clinet
    クライアント機能を別ファイルに分離

--- source/app.d
+++ source/app.d
@@ -1,6 +1,5 @@
 static import config = ekeymgr.config;
 import std.stdio;
-import std.string;
 import std.getopt;
 
 int main(string[] args)
@@ -60,53 +59,6 @@ int main(string[] args)
 		default:
 			break;
 	}
-	{
-		import std.socket;
-		import core.time;
-		import std.array;
-		auto address = new InternetAddress(config.ekeymgrServerAddress, config.ekeymgrServerPort);
-		auto socket = new TcpSocket(AddressFamily.INET);
-		socket.setOption(SocketOptionLevel.SOCKET, SocketOption.RCVTIMEO, dur!("seconds")(10));
-		socket.setOption(SocketOptionLevel.SOCKET, SocketOption.SNDTIMEO, dur!("seconds")(10));
-		try{
-			socket.connect(address);
-		}catch(SocketOSException e){
-			e.msg.writeln;
-			"Run \'systemctl start ekeymgr.service\' or \'ekeymgr daemon\' as root.".writeln;
-			return 1;
-		}
-		scope(exit) socket.close;
-		string str = args[1];
-		for(int i = 2; i < args.length; ++i){
-			str = str ~ " " ~ args[i];
-		}
-		socket.send(str);
-		auto buf = new char[255];
-		socket.receive(buf);
-		string[] receive = format(buf).split('\n');
-		if(receive.length == 0){
-			"error".writeln;
-			return 1;
-		}else{
-			string code = receive[0];
-			receive = receive[1..receive.length];
-			foreach(msg ; receive){
-				msg.writeln;
-			}
-			if(code != "0"){
-				return 1;
-			}
-		}
-	}
-	return 0;
-}
-string format(char[] buf){
-	for(int i = 0; i < buf.length; ++i){
-		if(buf[i] == 255){
-			buf = buf[0..i];
-			break;
-		}
-	}
-	import std.conv;
-	return buf.to!string.chomp;
+	static import client = ekeymgr.client;
+	return client.connect(args);
 }
